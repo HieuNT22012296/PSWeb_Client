bc3d5869a21578ce93dd2f4d964fa55ac2b842b5
HieuNT22012296 <nguyentrunghieu01082001@gmail.com>
2024-06-02 21:23:42 +0700

    update template

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+// import Register from "../pages/Register/Register";
+// import navbar from "../components/navbar/navbar";
+import Login from "../pages/Login/Login";
+
+export const routes = [
+  {
+    path: "/login",
+    page: Login,
+  },
+  // {
+  //   path: "/register",
+  //   page: Register,
+  // },
+];
